a5c242d26a30debc0ee00eb125e3d56c65a32852
Steve Tranby <stevetranby@gmail.com>
2016-03-06 22:17:17 -0700

    testing simple c++ struct (class) support

--- src/_test.cpp
+++ src/_test.cpp
@@ -1,3 +1,8 @@
+// TODO: probably should leave c++ to compiling for the full app target??
+// - though if we could use lambdas and a few other things that may be interesting?
+// - but it may just make the hardware interface more opaque
+// TODO: Possibly have a common system.h and then systemc.h and
+#include <system.h>
 #include <systemcpp.h>
 
 int addi(int a, int b) { return 42; }
@@ -14,3 +19,13 @@ void test() {
 	int* d = &c;
 	add(c,*d);
 }
+
+// Test compiling classes
+struct entity_component {};
+struct entity {
+    entity_component components[];
+};
+struct entity_system {
+    entity_component components_this_handles[];
+    void update(real64 dt);
+};
